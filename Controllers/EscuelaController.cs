91328ed222f4a0305fe3172da73c9f243682d5b4
Jose Ramon <joseviromay@gmail.com>
2021-02-08 12:44:21 +0100

    Comunicando modelo vista controlador

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,10 +1,21 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net.Models;
 
-namespace platzi_asp_net.Controllers {
-  public class EscuelaController : Controller {
-    public IActionResult Index(){
-      return View();
+namespace platzi_asp_net.Controllers
+{
+  public class EscuelaController : Controller
+  {
+
+    public IActionResult Index()
+    {
+      var escuela = new Escuela();
+      escuela.AñoFundacion = 2005;
+      escuela.Id = Guid.NewGuid().ToString();
+      escuela.Nombre = "Platzi School";
+      return View(escuela);
     }
+
   }
 }
 
---
+++ Models/Escuela.cs
@@ -0,0 +1,9 @@
+namespace platzi_asp_net.Models
+{
+  public class Escuela
+  {
+    public string Id { get; set; }
+    public string Nombre { get; set; }
+    public int AñoFundacion { get; set; }
+  }
+}
