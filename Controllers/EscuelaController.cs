2412c1fa5391b1d84211c5c134bdc86ba508f1f1
Jose Ramon <joseviromay@gmail.com>
2021-02-08 12:12:42 +0100

    Creando pagina escuela

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace platzi_asp_net.Controllers {
+  public class EscuelaController : Controller {
+    public IActionResult Index(){
+      return View();
+    }
+  }
+}
+
